d5bb89b3ffa5e01658f2be961a6e542debb9f4f4
Neto Martins <netomartins2@hotmail.com>
2022-02-09 16:06:45 -0300

    config: Listin providers

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.router';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
@@ -7,6 +9,7 @@ import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
+routes.use('/providers', providersRouter);
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
